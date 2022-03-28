7d5eb601bbfd534771922d93c4eaddfddd4341b5
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-03-27 23:37:06 -0700

    add timeTracker and ReadME

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Work Day Scheduler Starter Code
+As an employee with a busy schedule, I want to add important events to a daily planner so that i can manage my time effectively. 
+
+## Acceptance Criteria
+    This is a daily planner with the ability to create a schedule.
+    When you open the planner, the current day is displayed at the top.
+    When you scroll down, you are presented with time blocks for standard business hours. 
+    When you view the time blocks for that day, each time block is color-coded to indicate whether it is in the past, present and future.
+    When you click into the time-block, you can enter text and create a event.
+    when you click the save button for that time block,then the text for that event is saved in local storage.
+    When you refresh the page, the saved event stays on the schedule. 
+
+## Mock Up
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,47 @@
-//moment.js
 var currentDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").html(currentDate)
 
 $(document).ready(function() {
-    $(".saveBtn").on ("click", function() {
+    $(".saveBtn").on("click", function() {
         console.log ("click")
+        var text = $(this).siblings(".description").val;
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time,text)
     })
 })
+function timeTracker() {
+    var currentTime = moment().hour();
+    
+    $(".time-block").each(function() {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        if (blockTime < currentTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === currentTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })
+
+}
+
+$("#H8 .description").val(localStorage.getItem("hr8"));
+$("#H9 .description").val(localStorage.getItem("hr9"));
+$("#H10 .description").val(localStorage.getItem("hr10"));
+$("#H11 .description").val(localStorage.getItem("hr11"));
+$("#H12 .description").val(localStorage.getItem("hr12"));
+$("#H13 .description").val(localStorage.getItem("hr13"));
+$("#H14 .description").val(localStorage.getItem("hr14"));
+$("#H15 .description").val(localStorage.getItem("hr15"));
+$("#H16 .description").val(localStorage.getItem("hr16"));
+$("#H17 .description").val(localStorage.getItem("hr17"));
+
+timeTracker();
