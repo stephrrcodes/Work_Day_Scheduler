b9bd21dd2feb2f0902af281245db6dabd817afe0
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-06-22 00:25:42 -0700

    functional now

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,32 @@
+//display current date
 var currentDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").html(currentDate)
 
 $(document).ready(function() {
+    // listen for save button clicks
     $(".saveBtn").on("click", function() {
-        console.log ("click")
-        var text = $(this).siblings(".description").val;
+        //get nearby values
+        var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-        localStorage.setItem(time,text)
+
+        //save to localStorage
+        localStorage.setItem(time,text);
     })
 })
 function timeTracker() {
-    var currentTime = moment().hour();
+    //get current number of hours
+    var currentTime = moment().hours();
     
+    // loop over time blocks
     $(".time-block").each(function() {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        var blockTime = parseInt($(this).attr("id").split("-")[1]);
+
+        // check if we've moved time
         if (blockTime < currentTime) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
             $(this).addClass("past");
         }
         else if (blockTime === currentTime) {
             $(this).removeClass("past");
-            $(this).removeClass("future");
             $(this).addClass("present");
         }
         else {
@@ -29,19 +34,21 @@ function timeTracker() {
             $(this).removeClass("present");
             $(this).addClass("future");
         }
-    })
+    });
 
 }
-
-$("#H8 .description").val(localStorage.getItem("hr8"));
-$("#H9 .description").val(localStorage.getItem("hr9"));
-$("#H10 .description").val(localStorage.getItem("hr10"));
-$("#H11 .description").val(localStorage.getItem("hr11"));
-$("#H12 .description").val(localStorage.getItem("hr12"));
-$("#H13 .description").val(localStorage.getItem("hr13"));
-$("#H14 .description").val(localStorage.getItem("hr14"));
-$("#H15 .description").val(localStorage.getItem("hr15"));
-$("#H16 .description").val(localStorage.getItem("hr16"));
-$("#H17 .description").val(localStorage.getItem("hr17"));
-
 timeTracker();
+
+var interval = setInterval(timeTracker, 15000);
+
+// load any saved data from localStorage
+$("#H8 .description").val(localStorage.getItem("H8"));
+$("#H9 .description").val(localStorage.getItem("H9"));
+$("#H10 .description").val(localStorage.getItem("H10"));
+$("#H11 .description").val(localStorage.getItem("H11"));
+$("#H12 .description").val(localStorage.getItem("H12"));
+$("#H13 .description").val(localStorage.getItem("H13"));
+$("#H14 .description").val(localStorage.getItem("H14"));
+$("#H15 .description").val(localStorage.getItem("H15"));
+$("#H16 .description").val(localStorage.getItem("H16"));
+$("#H17 .description").val(localStorage.getItem("H17"));
